b9fe3f87490366eaabd6b622ab4d2e8012b59cc2
FARIS ISKANDAR BIN ZAMANI <farisiskandar@graduate.utm.my>
2026-01-01 18:07:03 +0800

    Login session time expiration
    cookie expiry date: 2 hours.

--- JS/Authenticate.js
+++ JS/Authenticate.js
@@ -1,25 +1,58 @@
 // checking valid user login session
-async function dynamicRedirect() {
-    if (!localStorage.getItem("TTMSFC_userSession")) {
-        let pathParts = window.location.pathname.split('/');
-        pathParts.pop(); 
+async function checkAuthentication() {
+    const sessionData = localStorage.getItem("TTMSFC_userSession");
+    const loginTime = localStorage.getItem("TTMSFC_loginTimestamp");
+    const currentTime = new Date().getTime();
+    
+    // Set login session expiry (HOURS * MINUTES * SECONDS * 1000ms)
+    // Default: 2 hours expiry date
+    const expiryDuration = 2 * 60 * 60 * 1000; 
 
-        while (pathParts.length > 0) {
-            let testPath = pathParts.join('/') + "/Login.html";
-            
-            try {
-                let response = await fetch(testPath, { method: 'HEAD' });
-                if (response.ok) {
-                    window.location.replace(testPath);
-                    return;
-                }
-            } catch (e) {
-            }
-            pathParts.pop();
-        }
+    let isInvalid = false;
+
+    // 1. Check if data exists
+    if (!sessionData || !loginTime) {
+        isInvalid = true;
+    } 
+    // 2. Check if the time has expired
+    else if (currentTime - parseInt(loginTime) > expiryDuration) {
+        alert("Your session has expired. Please login again.");
+        isInvalid = true;
+    }
+
+    if (isInvalid) {
+        // Clear anything left in storage to be safe
+        localStorage.removeItem("TTMSFC_userSession");
+        localStorage.removeItem("TTMSFC_loginTimestamp");
         
-        window.location.replace("../Login.html");
+        // Use your working dynamic redirect logic
+        await performDynamicRedirect();
+    }
+    else    {
+        localStorage.setItem("TTMSFC_loginTimestamp", new Date().getTime().toString());
     }
 }
 
-dynamicRedirect();
+async function performDynamicRedirect() {
+    let pathParts = window.location.pathname.split('/');
+    pathParts.pop(); 
+
+    while (pathParts.length > 0) {
+        let testPath = pathParts.join('/') + "/Login.html";
+        try {
+            let response = await fetch(testPath, { method: 'HEAD' });
+            if (response.ok) {
+                window.location.replace(testPath);
+                return;
+            }
+        } catch (e) {
+            // Fetch might fail on file:/// protocol, ignore and keep climbing
+        }
+        pathParts.pop();
+    }
+    
+    window.location.replace("../Login.html");
+}
+
+// Run the check
+checkAuthentication();
